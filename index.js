10f15d4f85bb03b6bfcdd53f1389188702fcf946
1804011 <sharifsharifulislam061@gmail.com>
2022-06-10 14:55:03 -0700

    project initialized

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(express.json());
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("welcome to our server");
+});
+app.listen(port, () => {
+  console.log("listening to port", port);
+});
