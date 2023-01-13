3983d2c6c1fd719a6180720e7a3a29a43858fbb8
1804011 <sharifsharifulislam061@gmail.com>
2023-01-13 11:32:14 +0600

    testing5

--- index.js
+++ index.js
@@ -20,10 +20,11 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-// app.get("/users", (req, res) => {
-//   console.log("ok");
-//   res.send({ pass: process.env.DB_PASS })
-// })
+app.get("/users", async (req, res) => {
+  const usersCollection = client.db("cse326").collection("users");
+  const result = await usersCollection.find().toArray();
+  res.send(result.reverse());
+});
 async function run() {
   try {
     await client.connect();
