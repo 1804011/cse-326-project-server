433d38807a092719b88f759c1bd430ddcefde634
1804011 <sharifsharifulislam061@gmail.com>
2023-03-15 17:26:46 +0600

    cors

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const port = process.env.PORT || 5000;
 
 //middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "*",
+
+}));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wf9uh.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
